3d1f35a13fa7544a52efad80ba4b8ad99e546801
tazlambert <Github@b7bf01>
2020-07-02 15:29:33 +0700

    initial commit

---
+++ demo_springboot_wls/src/main/java/com/example/springboot/wls/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.springboot.wls.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	String readResource() {
+		return "Hello World!";
+	}
+}
